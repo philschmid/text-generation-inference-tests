96e30d2877c9a36b7edc1c22e2179a1824e58ef9
Philipp Schmid <schmidphilipp1995@gmail.com>
2023-09-19 14:53:25 +0000

    start first light tests

--- sagemaker_llm_container/benchmark.py
+++ sagemaker_llm_container/benchmark.py
@@ -25,6 +25,7 @@ def parse_args():
 
 
 def run_benchmark(iam_role, token, model_id, instance_type, tp_degree, vu, quantize):
+    start = time.time()
     iam = boto3.client("iam")
     role = iam.get_role(RoleName=iam_role)["Role"]["Arn"]
 
@@ -55,52 +56,58 @@ def run_benchmark(iam_role, token, model_id, instance_type, tp_degree, vu, quant
     credentials = llm_model.sagemaker_session.boto_session.get_credentials()
 
     # deploy model to endpoint
-    llm = llm_model.deploy(
-        initial_instance_count=1,
-        instance_type=instance_type,
-        container_startup_health_check_timeout=300,
-    )
     try:
-        # run warm up inference
-        for i in range(2):
-            llm.predict({"inputs": "This is a sample sentence to warm up the model"})
-
-        # run k6 load test
-        time.sleep(10)
-        benchmark_start_time = time.time()
-        os.system(
-            f"k6 run sagemaker_load.js -e ENDPOINT_NAME={llm.endpoint_name} -e REGION={endpoint_region} -e DO_SAMPLE=0 -e VU={vu} -e AWS_ACCESS_KEY_ID={credentials.access_key} -e AWS_SECRET_ACCESS_KEY={credentials.secret_key}"
-        )
-
-        # wait for cloudwatch logs to be populated and ready to read
-        print("Waiting for cloudwatch logs to be populated")
-        time.sleep(120)
-        benchmark_end_time = time.time()
-
-        # get cloudwatch logs
-        results = get_metrics_from_cloudwatch(
-            endpoint_name=llm.endpoint_name,
-            st=int(benchmark_start_time),
-            et=int(benchmark_end_time),
+        llm = llm_model.deploy(
+            initial_instance_count=1,
             instance_type=instance_type,
-            tp_degree=tp_degree,
-            vu=vu,
-            quantize=quantize,
-            model_id=model_id,
+            container_startup_health_check_timeout=300,
         )
+        try:
+            # run warm up inference
+            for i in range(2):
+                llm.predict({"inputs": "This is a sample sentence to warm up the model"})
 
-        # print results
-        with open(
-            f"{model_id.split('/')[-1]}_{instance_type}_tp_{int(tp_degree)}_vu_{int(vu)}.json",
-            "w",
-        ) as f:
-            f.write(json.dumps(results))
+            # run k6 load test
+            time.sleep(10)
+            benchmark_start_time = time.time()
+            os.system(
+                f"k6 run sagemaker_load.js -e ENDPOINT_NAME={llm.endpoint_name} -e REGION={endpoint_region} -e DO_SAMPLE=0 -e VU={vu} -e AWS_ACCESS_KEY_ID={credentials.access_key} -e AWS_SECRET_ACCESS_KEY={credentials.secret_key}"
+            )
+
+            # wait for cloudwatch logs to be populated and ready to read
+            print("Waiting for cloudwatch logs to be populated")
+            time.sleep(120)
+            benchmark_end_time = time.time()
+
+            # get cloudwatch logs
+            results = get_metrics_from_cloudwatch(
+                endpoint_name=llm.endpoint_name,
+                st=int(benchmark_start_time),
+                et=int(benchmark_end_time),
+                instance_type=instance_type,
+                tp_degree=tp_degree,
+                vu=vu,
+                quantize=quantize,
+                model_id=model_id,
+            )
+
+            # print results
+            with open(
+                f"{model_id.split('/')[-1]}_{instance_type}_tp_{int(tp_degree)}_vu_{int(vu)}.json",
+                "w",
+            ) as f:
+                f.write(json.dumps(results))
+        except Exception as e:
+            print(e)
+        finally:
+            # delete endpoint
+            llm.delete_model()
+            llm.delete_endpoint()
     except Exception as e:
         print(e)
+        print(f"Failed to deploy model with config {model_id}, {instance_type}, {tp_degree}, {vu}")
     finally:
-        # delete endpoint
-        llm.delete_model()
-        llm.delete_endpoint()
+        print(f"Total time: {round(time.time() - start)}s")
 
 
 if __name__ == "__main__":
@@ -112,6 +119,11 @@ if __name__ == "__main__":
 
         # run each config for each vu group
         vu_group = [1, 5, 10, 20]
+
+        # run each config for each vu group
+        print(f"Running benchmark for {len(configs) * 4} configs")
+        print(f"Expected time between: {len(configs) * 4 * 18}-{len(configs) * 4 * 27} minutes")
+
         for config in configs:
             for vu in vu_group:
                 run_benchmark(
